498e1c093eb2d87757809000bcf76a7e05e68724
ouedraogoclodia <ouedraogoclodia2001@gmail.com>
2022-08-09 14:30:29 +0000

    first_commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,19 +8,17 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero ={
-    id : 1,
-    name : 'Windstorm',
-  };
+  
   heroes: Hero[]=[];
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes =>this.heroes = heroes);
+  }
 
   ngOnInit(): void {
-    
+    this.getHeroes();
   }
-  selectedHero?: Hero;
-  onSelect(hero:Hero): void{
-    this.selectedHero = hero;
-  }
+  
 
 }
