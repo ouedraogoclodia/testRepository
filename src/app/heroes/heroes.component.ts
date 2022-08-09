38892f302803508181810e890a5b3b12fd449c87
ouedraogoclodia <ouedraogoclodia2001@gmail.com>
2022-08-09 11:01:10 +0000

    firstTest

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero : Hero ={
+    id : 1,
+    name : 'Windstorm',
+  };
+  heroes: Hero[]=[];
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  selectedHero?: Hero;
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dr. Nice'},
+    {id: 13, name: 'Celeritas'},
+    {id: 14, name: 'Magneta'},
+    {id: 15, name: 'RubberMan'},
+    {id: 16, name: 'Dynama'},
+    {id: 17, name: 'Dr. IQ'},
+    {id: 18, name: 'Magma'},
+    {id: 19, name: 'Tornado'},
+    {id: 20, name: 'Bombasto'},
+];
